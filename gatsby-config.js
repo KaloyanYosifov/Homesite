db9b53362820156b38d025452c3b43d62976ebe0
KaloyanYosifov <kaloqn665@gmail.com>
2021-09-05 07:55:41 +0300

    Replace google fonts plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,14 +39,12 @@ module.exports = {
             },
         },
         {
-            resolve: `gatsby-plugin-prefetch-google-fonts`,
+            resolve: `gatsby-plugin-google-fonts`,
             options: {
                 fonts: [
-                    {
-                        family: `Rubik`,
-                        variants: [`400`, '500', `700`, '900'],
-                    },
+                    'Rubik\:400,500,700,900',
                 ],
+                display: 'swap',
             },
         },
         'gatsby-transformer-remark',
